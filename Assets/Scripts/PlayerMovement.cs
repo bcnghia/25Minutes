27674071a561e2f36b2ac117a8bdac99daa1be6c
Bui Cong Nghia <86724705+bui-cong-nghia@users.noreply.github.com>
2023-10-12 03:08:32 +0700

    Add Cat_Girl to the GAME

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveDir;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        InputManagement();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+}
