5c5f50128bda03355cb8b96c4293e4326934516c
khangksp <129304975+khangksp@users.noreply.github.com>
2023-10-17 18:43:10 +0700

    Tăng hitbox của nhân vật, Nhặt vật phẩm, more items added

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Loot"))
+        {
+            PickUp lootItem = other.GetComponent<PickUp>();
+            if (lootItem != null)
+            {
+                lootItem.CollectLoot();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Collected Item");
+            CollectLoot();
+        }
+    }
+    public void CollectLoot()
+    {
+        // Xử lý khi vật phẩm được nhặt
+        // Tăng kinh nghiệm, hồi máu, tăng tốc, buff, etc,....
+        Destroy(gameObject); 
+    }
+}
