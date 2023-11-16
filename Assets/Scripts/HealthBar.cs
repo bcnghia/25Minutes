11c914e5cdecc780e73325877e7e2d08fc094bd4
Bui Cong Nghia <86724705+bui-cong-nghia@users.noreply.github.com>
2023-11-16 12:53:04 +0700

    HOÀN THIỆN

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class HealthBar : MonoBehaviour
 	[SerializeField] private Slider slider;
     [SerializeField] private Gradient gradient;
     [SerializeField] private Image fill;
-    [SerializeField] private Text healthText;
+    [SerializeField] private TMP_Text healthText;
 
 	//public void SetMaxHealth(float health)
 	//{
@@ -20,7 +21,7 @@ public class HealthBar : MonoBehaviour
 
     public void SetHealth(float health, float maxHealth)
 	{
-		slider.maxValue = health;
+		slider.maxValue = maxHealth;
 		slider.value = health;
         healthText.text = health.ToString() + " / " + maxHealth.ToString();
         fill.color = gradient.Evaluate(slider.normalizedValue);
